7ef9d08e3dbc015f778fe59effbd33a0c1744bd1
Erwin Feng <efeng732@gmail.com>
2020-11-09 21:21:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
    :home => {
     :team_name => "Brooklyn Nets", 
     :colors => [], 
-    :players => [{:player_name}, {:number}, {:shoe}, {:points}, {:rebounds}, {:assists},{:steals},{:blocks},{:slam_dunks} ]
+    :players => [{:player_name, :number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks}]
    },
    
    :away => {
