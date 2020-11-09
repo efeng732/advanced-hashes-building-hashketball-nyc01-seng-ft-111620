414f8fb684cab9f8be528efabf0871c5d5e816e8
Erwin Feng <efeng732@gmail.com>
2020-11-09 21:19:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,6 +6,7 @@ def game_hash
     :colors => [], 
     :players => [{:player_name}, {:number}, {:shoe}, {:points}, {:rebounds}, {:assists},{:steals},{:blocks},{:slam_dunks} ]
    },
+   
    :away => {
      :team_name => "", 
      :colors => [], 
